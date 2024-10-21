f2ca48e85269a1059c1395d1ea30024c40dcbc21
alexwlad3885 <alexwlad3885@yandex.ru>
2024-10-21 21:08:24 +0300

    Домашнее задание по теме "Urls и Views. Функциональное и классовое представление."

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from task2.views import func_perf, ClassPerf
 
+# urlpatterns = [
+#     path('', func_template),
+#     path('page1/', class_template),
+# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_perf),
+    path('proba/', ClassPerf.as_view()),
 ]
