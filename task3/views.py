74b0ccda89e98ccf5e2fc0d3b0477255f0874e41
alexwlad3885 <alexwlad3885@yandex.ru>
2024-10-23 21:49:32 +0300

    Домашнее задание по теме "Базовые HTML тэги в шаблонах".Доработка

--- task3/views.py
+++ task3/views.py
@@ -6,7 +6,15 @@ def func_index(request):
 
 
 def func_index_1(request):
-    return render(request, 'index_1.html')
+    point_1 = 'Игра для взрослых'
+    point_2 = 'Игра для подростков'
+    point_3 = 'Игра для детей'
+    context = {
+        'point_1': point_1,
+        'point_2': point_2,
+        'point_3': point_3,
+    }
+    return render(request, 'index_1.html', context)
 
 
 def func_index_2(request):
